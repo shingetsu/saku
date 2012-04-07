69675eb5cd4f0d5189160d6c1b0ebe741d7b8ccc
Satoshi Fukutomi <info@fuktommy.com>
2012-04-07 21:19:06 +0900

    use pretty format for version name

--- tool/git2ver.sh
+++ tool/git2ver.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # git log format to version string
 #
-env LANG=C git log -n 1 |
-    awk '/^Date:/ { print $2 " " $3 " " $4 " " $5  " " $7 " " $6}' |
+git log -n 1 --pretty=format:"%ai" |
     env TZ=UTC xargs -0 date +'%Y%m%d-%H%M' --date
