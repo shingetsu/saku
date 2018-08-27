72589676cb013cec0ef87a10196116c6a1899689
Satoshi Fukutomi <info@fuktommy.com>
2018-08-27 10:33:48 +0900

    release 4.8.2

--- shingetsu/config.py
+++ shingetsu/config.py
@@ -48,7 +48,7 @@ def _get_value(parser, section, key, default, vtype=''):
 def _get_version():
     """Get Saku version for useragent and servername.
     """
-    version = '4.8.1'
+    version = '4.8.2'
     version_file = os.path.join(docroot, file_dir, 'version.txt')
     if os.path.isfile(version_file):
         try:
