328547b519347676299d5012e3c2079601d5c6ed
Satoshi Fukutomi <info@fuktommy.com>
2025-02-22 10:28:25 +0900

    static files

--- shingetsu/httpd.py
+++ shingetsu/httpd.py
@@ -26,6 +26,8 @@
 # SUCH DAMAGE.
 #
 
+import http
+import mimetypes
 import os
 import socket
 import socketserver
@@ -42,8 +44,10 @@ from . import middleware
 @middleware.last_modified
 @middleware.gzipped
 def root_app(environ, start_response):
-    log_request(environ)
     path = environ.get('PATH_INFO', '')
+
+    if not path or path == '/':
+        path = config.root_index
 
     #routes = [
     #    (board_re, board_app),
@@ -59,20 +63,15 @@ def root_app(environ, start_response):
     #            env['mch.path_match'] = m
     #            return app(env, resp)
 
-    start_response("404 Not Found", [('Content-Type', 'text/plain')])
+    docroot = os.path.abspath('.')
+    filepath = os.path.join(docroot, path.lstrip('/'))
+    if filepath.startswith(docroot) and os.path.isfile(filepath):
+        filetype, _ = mimetypes.guess_type(filepath)
+        start_response('200 OK', [('Content-Type', filetype)])
+        return environ['wsgi.file_wrapper'](open(filepath, 'rb'), 1024)
+
+    start_response('404 Not Found', [('Content-Type', 'text/plain')])
     return [b'404 Not Found']
-
-
-def log_request(env):  # same as saku's log format
-    fmt = '{host}<>{proxy}<>{method} {path} {protocol}<>{referer}<>{ua}'
-    msg = fmt.format(host=env['REMOTE_ADDR'],
-                     proxy=env.get('HTTP_X_FORWARDED_FOR', 'direct'),
-                     method=env['REQUEST_METHOD'],
-                     path=env['PATH_INFO'],
-                     protocol=env['SERVER_PROTOCOL'],
-                     referer=env.get('REFERER', ''),
-                     ua=env.get('HTTP_USER_AGENT', ''))
-    sys.stdout.write(msg)
 
 class Httpd(threading.Thread):
     """Tiny HTTP server running in another thread.
@@ -83,20 +82,40 @@ class Httpd(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
-        class Server(socketserver.ThreadingMixIn,
-                     simple_server.WSGIServer):
-            pass
-
         if not config.bind_addr or ':' in config.bind_addr:
             Server.address_family = socket.AF_INET6
-        Server.server_version = config.version
         
         self.server = simple_server.make_server(
             config.bind_addr,
             config.port,
             root_app,
-            server_class=Server)
+            server_class=Server,
+            handler_class=RequestHandler)
 
     def run(self):
         os.chdir(config.docroot)
+        simple_server.ServerHandler.server_software = config.version
         self.server.serve_forever()
+
+
+class Server(socketserver.ThreadingMixIn, simple_server.WSGIServer):
+    pass
+
+
+class RequestHandler(simple_server.WSGIRequestHandler):
+    def log_request(self, code='-', size='-'):
+        buf = [self.requestline]
+        if hasattr(self, "headers"):
+            buf.extend((self.headers.get("Referer", ""),
+                        self.headers.get("User-Agent", "")))
+        self.log_message("%s", "<>".join(buf))
+
+    def log_message(self, format, *args):
+        if hasattr(self, 'headers'):
+            proxy_client = self.headers.get('X-Forwarded-For', 'direct')
+        else:
+            proxy_client = '?'
+        sys.stderr.write('%s<>%s<>%s\n' %
+                         (self.address_string(),
+                          proxy_client,
+                          format % args))
